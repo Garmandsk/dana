14f7c1784eb43b82234ab70a770649fa69074e17
Garmandsk <xmiagertiarmanda@gmail.com>
2024-12-29 10:25:33 +0700

    fe utama selesai

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+const Hapi = require('@hapi/hapi');
+
+require('dotenv').config();
+const { PORT, HOST } = process.env;
+
+/*
+const routes = require('./routes');
+*/
+
+const init = async () => {
+    const server = Hapi.server({
+        port: PORT,
+        host: HOST,
+        // tanpa cors kita tidak dapat menyimpan data dari frontend yang deploy
+        
+        /*
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+        */
+        
+    });
+    
+    /*
+    server.route(routes);
+    */
+
+    await server.start();
+
+    console.log(`Server running on port ${server.info.uri}`);
+};
+
+init();

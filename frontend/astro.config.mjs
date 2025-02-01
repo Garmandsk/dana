844af5f98281ae31a516a05e5823c8e6d5a296b7
Garmandsk <xmiagertiarmanda@gmail.com>
2025-02-01 21:18:34 +0700

    Auth fe Selesai betul

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-import { onRequest } from "./src/middleware.ts";
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  middleware: onRequest,
   output: 'server',
   adapter: node({
     mode: 'standalone',
@@ -23,7 +21,8 @@ export default defineConfig({
   },
   env: {
     schema: {
-      API_URL_HAPI: envField.string({ context: "client", access: "public", optional: true }),
+      API_BE: envField.string({ context: "client", access: "public", optional: true }),
+      KATA_SANDI: envField.string({ context: "client", access: "public", optional: true }),
       /* API_PORT_HAPI: envField.number({ context: "server", access: "public", default: 5000 }),
       API_HOST_HAPI: envField.string({ context: "server", access: "public", default: "localhost" }),
       API_SECRET: envField.string({ context: "server", access: "secret" }), */
